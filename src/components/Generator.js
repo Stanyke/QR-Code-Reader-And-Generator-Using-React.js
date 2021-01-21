3f3e9c303a791ab226bb6893ea5d2abf18b04990
Stanyke <Chibuike.stanley50@gmail.com>
2021-01-21 15:07:03 +0100

    Installed components

---
+++ src/components/Generator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Generator extends Component {
+    render() {
+        return (
+            <div>
+                <h2 class="text-center">QR Code Generator</h2>
+
+            
+            </div>
+        )
+    }
+}
