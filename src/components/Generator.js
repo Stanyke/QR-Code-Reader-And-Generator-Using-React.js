873ef8d9f68b3327e19d1a2d3dff02752b8f6b88
Stanyke <Chibuike.stanley50@gmail.com>
2021-01-21 15:45:59 +0100

    Installed components

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -10,30 +10,50 @@ export default class Generator extends Component {
             userInput: ""
         }
     }
+
+    componentDidMount(){
+        document.getElementById('failedParagraph').style.display = 'none'
+    }
     
     render() {
         return (
             <div className="container">
-                <h2 class="text-center">QR Code Generator</h2>
 
-                <input type="text" onChange={(e) => this.setState({userInput: e.target.value})} className="form-control" required />
+                <div class="generatorPage">
+                    
+                    <h2 class="text-center text-primary">QR Code Generator</h2>
 
-                <button type="button" onClick={() => this.submitInputValue()} className="mt-4 form-control btn btn-danger">Submit</button>
+                    <form className="pt-3" onSubmit={this.submitInputValue}>
+                        <input type="text" onChange={(e) => this.setState({userInput: e.target.value})} className="form-control" required />
 
-                <canvas id="canvas" className="mt-3"></canvas>
+                        <button type="submit" className="mt-4 form-control btn btn-danger">Submit</button>
+                    </form>
+
+                    <canvas id="canvas" className="mt-3" align="center"></canvas>
+                    
+                    <p className="mt-3" id="failedParagraph">Failed to load QR Code, try again.</p>
+
+                </div>
             </div>
         )
     }
 
-    submitInputValue = () => {
-        var canvas = document.getElementById('canvas')
- 
+    submitInputValue = (event) => {
+        event.preventDefault();
+
+        let canvas = document.getElementById('canvas')
+        let failedParagraph = document.getElementById('failedParagraph')
+
+        
         QRCode.toCanvas(canvas, this.state.userInput, (error) => {
             if(error){
                 console.error(error)
+                canvas.style.display = 'none';
+                failedParagraph.style.display = 'block'
             }
             else{
-                console.log('success!');
+                canvas.style.display = 'block';
+                failedParagraph.style.display = 'none'
             }
         })
     }
